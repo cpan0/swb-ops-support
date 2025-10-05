8fa554e9a03e4d71fbc00aff400390ea55025592
Clare <clarepan0@gmail.com>
2025-10-05 16:36:37 -0700

    refactor scraper class

--- scraper.py
+++ scraper.py
@@ -2,57 +2,96 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) '\
-'AppleWebKit/537.36 (KHTML, like Gecko) '\
-           'Chrome/75.0.3770.80 Safari/537.36'}
+class NGOScraper:
+  def __init__(self):
+    self.base_url = "https://ngobase.org/"
+    self.header = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) '\
+                   'AppleWebKit/537.36 (KHTML, like Gecko) '\
+                    'Chrome/75.0.3770.80 Safari/537.36'}
+    self.countries = None
 
-base_url = "https://ngobase.org/"
-url = "https://ngobase.org/c/GF/french-guina-ngos-charities"
+  # get all available country and return a dictionary {country : abbrv}
+  def get_countries(self) -> dict:
+    response = requests.get(self.base_url, headers=self.header)
+    soup = BeautifulSoup(response.text, "html.parser")
 
-data = []
+    results = soup.find(id="ngo_country")
 
-while url: 
-  print(f"Scraping {url}")
+    options = results.find_all("option")
 
-  page = requests.get(url, headers=headers)
-  soup = BeautifulSoup(page.content, "html.parser")
+    countries = {}
 
-  results = soup.find(id="main_content")
+    for op in options:
+      key = op.text.lower()
 
-  if not results:
-    break
+      if key:
+        countries[key] = op["value"]
 
-  postings = results.find_all("div", class_="ngo_listing_div")
+    self.countries = countries
 
-  for p in postings:
-    # Find NGO name
-    name = None
-    name = p.find("h3", class_="ngo_name").find("a").text
+  def get_country_ngo(self, country: str):
+    country = country.lower()
+    if country in self.countries:
+      path = country.replace(" ", "-")
+      url = self.base_url + "c/" + self.countries[country] + "/" + path + "-ngos-charities"
+      print("Retrieving from: " + url)
 
-    # Find NGO website link
-    link = None
-    web_elm = p.find("img", alt="NGO website")
-    if web_elm:
-      link_elm = web_elm.find_parent("a")
-      if link_elm and link_elm.has_attr("href"):
-        link = link_elm["href"]
-    
-    if name or link:
-      data.append({"name": name, "website": link})
+      data = []
+  
+      while url: 
+        page = requests.get(url, headers=self.header)
+        soup = BeautifulSoup(page.content, "html.parser")
 
-  # Find next url
-  next_url = None
-  pagination = results.find("ul", class_="pagination")
-  next_link = pagination.find("a", {"rel" : "next"})
-  if next_link and next_link.has_attr("href"):
-    next_url = next_link["href"]
+        results = soup.find(id="main_content")
 
-  url = next_url
+        if not results:
+          break
 
-print("Total NGOs collected:", len(data))
+        postings = results.find_all("div", class_="ngo_listing_div")
 
-with open('data/ppp.csv', 'w', newline='') as csvfile:
-    fieldnames = ['name', 'website']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    writer.writeheader()
-    writer.writerows(data)
+        for p in postings:
+          # Find NGO name
+          name = None
+          name = p.find("h3", class_="ngo_name").find("a").text
+
+          # Find NGO website link
+          link = None
+          web_elm = p.find("img", alt="NGO website")
+          if web_elm:
+            link_elm = web_elm.find_parent("a")
+            if link_elm and link_elm.has_attr("href"):
+              link = link_elm["href"]
+          
+          if name or link:
+            data.append({"name": name, "website": link})
+
+        # Find next url
+        next_url = None
+        pagination = results.find("ul", class_="pagination")
+        next_link = pagination.find("a", {"rel" : "next"})
+        if next_link and next_link.has_attr("href"):
+          next_url = next_link["href"]
+
+        url = next_url
+
+      print("Total NGOs collected:", len(data))
+      self.save_data(data, path)
+
+    else: 
+      print("Error: invalid url")
+
+  def save_data(self, data, path: str):
+    print("Saving data")
+
+    with open("data/" + path, "w", newline="") as csvfile:
+      fieldnames = ['name', 'website']
+      writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+      writer.writeheader()
+      writer.writerows(data)
+
+
+if __name__ == "__main__":
+  scraper = NGOScraper()
+  scraper.get_countries()
+  print(len(scraper.countries))
+  scraper.get_country_ngo("iceland")
